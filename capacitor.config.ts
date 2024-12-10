ed82bc83a627a34acf1c178f18719a906909237a
Freitassync <gabrielfreitas3010@outlook.com>
2024-12-10 15:01:22 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Freitas',
+  webDir: 'dist'
+};
+
+export default config;
